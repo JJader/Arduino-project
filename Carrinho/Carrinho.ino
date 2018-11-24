efcd8004b7652bcbdbd5ab7311c184ccae60fc8f
JJamisson <JJader03@gmail.com>
2018-11-23 23:31:54 -0200

    Atualizando velocimetro

--- Carrinho/Carrinho.ino
+++ Carrinho/Carrinho.ino
@@ -8,6 +8,8 @@ int IN3 = 6;
 int IN4 = 7;
 #define led 8
 #define buz 9
+
+int veloA = 11, veloB = 10, fat = 0;
 char comando;
 
 void setup() {
@@ -19,6 +21,8 @@ void setup() {
   pinMode(IN4, OUTPUT);
   pinMode(led, OUTPUT);
   pinMode(buz, OUTPUT);
+  pinMode(veloB, OUTPUT);
+  pinMode(veloA, OUTPUT);
   delay(3000);
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
@@ -32,42 +36,73 @@ void setup() {
 
 void loop() {
    while (bluetooth.available() || Serial.available()) {
-    
-    if (bluetooth.available()){
-    comando = bluetooth.read();
-    }else{
-    comando = Serial.read();
-    }
+    if (bluetooth.available()){comando = bluetooth.read();}
+    else{comando = Serial.read();}
 
-
-    
     Serial.println(comando);
     
-    if (comando == 'W'){
-      funcLed(HIGH);
+    analogWrite(veloA, fat);
+    analogWrite(veloB, fat);
+
+    switch(comando){
+
+        case '0':
+        fat = 0;
+        break;
+        
+        case '1':
+        fat = 25.5*1;
+        break;
+        
+        case '2':
+        fat = 25.5*2;
+        break;
+        
+        case '3':
+        fat = 25.5*3;
+        break;
+        
+        case '4':
+        fat = 25.5*4;
+        break;
+        
+        case '5':
+        fat = 25.5*5;
+        break;
+        
+        case '6':
+        fat = 25.5*6;
+        break;
+        
+        case '7':
+        fat = 25.5*7;
+        break;
+        
+        case '8':
+        fat = 25.5*8;
+        break;
+        
+        case '9':
+        fat = 25.5*9;
+        break;
+        
+        case 'q':
+        fat = 25.5*10;
+        break;
+
+     default:
+    if (comando == 'W'){funcLed(HIGH);}
+    if (comando == 'w'){funcLed(LOW);}
+    if (comando == 'V' || comando == 'v'){funcBuz();}
+    if (comando == 'F') {frente();}
+    else if (comando == 'B') {tras();}
+    else if (comando == 'L') {esquerda();}
+    else if (comando == 'R') {direita();}
+    else { parado();}
+    break;
     }
-    if (comando == 'w'){
-      funcLed(LOW);
-    }
-    if (comando == 'V' || comando == 'v'){
-      funcBuz();
-    }
-    if (comando == 'F') {
-      frente();
-    }
-    else if (comando == 'B') {
-      tras();
-    }
-    else if (comando == 'L') {
-      esquerda();
-    }
-    else if (comando == 'R') {
-      direita();
-    } else {
-      parado();
-    }
-  }
-}
+    
+  }}
 
 
 void funcLed(bool a){
